2a2404aa7f3bcc5f4b26562a0acf790685a3118f
Arthur Zaczek <arthur@dasz.at>
2012-03-23 12:57:59 +0100

    new zbpull versions

--- ZbPullPrebuiltBinaries.cmd
+++ ZbPullPrebuiltBinaries.cmd
@@ -4,4 +4,4 @@ echo Pull the binaries and scripts from the build server.
 echo Use this to update your local environment to the current build server binaries.
 echo ********************************************************************************
 
-ZbPull.cmd \\SRV2008\Data\CruiseControl.NET\Projects\ZBox-WPF-artifacts\Latest Libs\Kistl
+ZbPull.cmd "\\srv2008\data\Jenkins\workspace\zetbox - Windows - develop" Libs\Kistl
