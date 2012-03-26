de425af6fb97033e5adba72069b76e05f8530017
Arthur Zaczek <arthur@dasz.at>
2012-03-26 10:30:45 +0200

    Custom Actions in QuoteItems for updating AmountNet, etc.

--- Kistl.Parties.Client/Invoicing/QuoteItemActions.cs
+++ Kistl.Parties.Client/Invoicing/QuoteItemActions.cs
@@ -11,11 +11,40 @@ namespace ZBox.Basic.Invoicing
     public static class QuoteItemActions
     {
         [Invocation]
-        public static void postSet_UnitPrice(QuoteItem obj, PropertyPostSetterEventArgs<decimal?> e)
+        public static void postSet_Quantity(QuoteItem obj, PropertyPostSetterEventArgs<decimal> e)
         {
-            if (obj.UnitPrice.HasValue && obj.Amount == 0)
+            if (obj.UnitPrice.HasValue)
             {
                 obj.AmountNet = obj.Quantity * obj.UnitPrice.Value;
+            }
+        }
+
+        [Invocation]
+        public static void postSet_UnitPrice(QuoteItem obj, PropertyPostSetterEventArgs<decimal?> e)
+        {
+            if (obj.UnitPrice.HasValue)
+            {
+                obj.AmountNet = obj.Quantity * obj.UnitPrice.Value;
+            }
+        }
+
+        [Invocation]
+        public static void postSet_AmountNet(QuoteItem obj, PropertyPostSetterEventArgs<decimal> e)
+        {
+            if (obj.VATType != null)
+            {
+                var percent = (obj.VATType.Percentage ?? (decimal)0) / (decimal)100;
+                obj.Amount = obj.AmountNet + (obj.AmountNet * percent) + (obj.VATType.Absolute ?? (decimal)0);
+            }
+        }
+
+        [Invocation]
+        public static void postSet_VATType(QuoteItem obj, PropertyPostSetterEventArgs<VATType> e)
+        {
+            if (obj.VATType != null)
+            {
+                var percent = (obj.VATType.Percentage ?? (decimal)0) / (decimal)100;
+                obj.Amount = obj.AmountNet + (obj.AmountNet * percent) + (obj.VATType.Absolute ?? (decimal)0);
             }
         }
     }
