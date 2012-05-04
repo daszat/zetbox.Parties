23fbc28caecb2db1a9a95509388edd690f02b0a9
Arthur Zaczek <arthur@dasz.at>
2012-05-04 12:22:49 +0200

    new zetbox version; new source for prebuild binaries

--- ZbPullPrebuiltBinaries.cmd
+++ ZbPullPrebuiltBinaries.cmd
@@ -4,4 +4,4 @@ echo Pull the binaries and scripts from the build server.
 echo Use this to update your local environment to the current build server binaries.
 echo ********************************************************************************
 
-ZbPull.cmd "\\srv2008\data\Jenkins\workspace\zetbox-develop-Windows-Deploy\deployed" Libs\Kistl
+ZbPull.cmd "\\srv2008\data\Jenkins\workspace\zetbox-develop-Windows-Deploy_update\deployed" Libs\Kistl
