3b9620b9da8cf204636ac297a1c730bbb63175e9
Arthur Zaczek <arthur@dasz.at>
2012-09-28 10:28:15 +0200

    Starting with case 7804: Receipt Templates

---
+++ Zetbox.Parties.Common/Invoicing/ReceiptTemplateActions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zetbox.API;
+
+namespace Zetbox.Basic.Invoicing
+{
+    [Implementor]
+    public static class ReceiptTemplateActions
+    {
+        //[Invocation]
+        //public static void ToString(ReceiptTemplate obj, MethodReturnEventArgs<System.String> e)
+        //{
+        //    e.Result = string.Format("{0}, {1}, Total {2}/{3}", obj.Description, obj.Period, obj.TotalNet, obj.Total);
+        //}
+
+        //[Invocation]
+        //public static void CreateReceipt(ReceiptTemplate obj, MethodReturnEventArgs<Zetbox.Basic.Invoicing.Receipt> e)
+        //{
+        //}
+    }
+}
