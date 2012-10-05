127410a47e0ab7031fb2f0bb100f751d724349ec
Arthur Zaczek <arthur@dasz.at>
2012-10-05 11:28:05 +0200

    Adding missing relations and methods to receipt templates

---
+++ Zetbox.Parties.Common/Invoicing/OtherExpenseReceiptTemplateActions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zetbox.API;
+
+namespace Zetbox.Basic.Invoicing
+{
+    [Implementor]
+    public static class OtherExpenseReceiptTemplateActions
+    {
+        [Invocation]
+        public static void CreateReceipt(OtherExpenseReceiptTemplate obj, MethodReturnEventArgs<Zetbox.Basic.Invoicing.Receipt> e)
+        {
+            var ctx = obj.Context;
+            var result = ctx.Create<OtherExpenseReceipt>();
+
+            result.Date = DateTime.Today;
+            result.Description = obj.Description;
+            result.Document = obj.Document;
+            result.DueDate = obj.DueDate.GetCurrent(result.Date);
+            result.IntOrg = obj.IntOrg;
+            result.Message = obj.Message;
+            result.Party = obj.Party;
+            result.Period = obj.Period;
+            result.Total = obj.Total;
+            result.TotalNet = obj.TotalNet;
+
+            e.Result = result;
+        }
+    }
+}
