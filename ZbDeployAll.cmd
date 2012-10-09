c9788f157a49c004e8520558127ef7bf2f31944b
David Schmitt <david@dasz.at>
2012-10-09 16:48:58 +0200

    Build: improve zbresetall/zbdeploy to manage fallback correctly and compile the solution when required

--- ZbDeployAll.cmd
+++ ZbDeployAll.cmd
@@ -5,23 +5,30 @@ echo Changes to the object model are generated.
 echo Use this to apply upstream changes.
 echo ********************************************************************************
 
+set fallbackconfig=
 set config=
 
 if .%1. == .. GOTO GOON
-
+set fallbackconfig=%1
 set config=%1
+
+if .%2. == .. GOTO GOON
+set config=%2
 
 :GOON
 
-call "ZbInstall.cmd" %config%
+call "ZbInstall.cmd" %fallbackconfig%
 
 cd bin\Debug
 
-Zetbox.Server.Service.exe %config% -deploy-update -generate
+Zetbox.Server.Service.exe %fallbackconfig% -deploy-update -generate
 IF ERRORLEVEL 1 GOTO FAIL
 
 cd ..\..
-call "ZbInstall.cmd" %config%
+
+msbuild zetbox.Parties.sln
+IF ERRORLEVEL 1 GOTO FAIL2
+
 cd bin\Debug
 
 Zetbox.Server.Service.exe %config% -import Data\Workflow.Data.xml
@@ -35,11 +42,12 @@ cd ..\..
 GOTO EOF
 
 :FAIL
+cd ..\..
+:Fail2
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX FAIL XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 echo                                  Aborting Deploy
-cd ..\..
 rem return error without closing parent shell
 echo A | choice /c:A /n
 
