f8b392c38cd70abf7185bdb751c290d0311940fb
Arthur Zaczek <arthur@dasz.at>
2012-10-12 11:51:04 +0200

    case 8070: Implementing a very simple match algo for assigning transactions & parties

--- Zetbox.Parties.Common/Accounting/AccountActions.cs
+++ Zetbox.Parties.Common/Accounting/AccountActions.cs
@@ -5,6 +5,7 @@ namespace Zetbox.Basic.Accounting
     using System.Linq;
     using System.Text;
     using Zetbox.API;
+    using Zetbox.Basic.Parties;
 
     [Implementor]
     public static class AccountActions
@@ -13,6 +14,60 @@ namespace Zetbox.Basic.Accounting
         public static void ToString(Account obj, MethodReturnEventArgs<System.String> e)
         {
             e.Result = obj.Name;
+        }
+
+        [Invocation]
+        public static void AssignParties(Account obj)
+        {
+            var ctx = obj.Context;
+            // TODO: Improve this!
+            var allParties = ctx.GetQuery<Party>()
+                .ToList()
+                .Select(p => new { Party = p, Names = GetPartyNames(p) })
+                .Where(p => p.Names.Length > 0)
+                .ToList();
+            foreach (var transaction in ctx.GetQuery<Transaction>().Where(t => t.Party == null))
+            {
+                var comment = transaction.Comment.ToLower();
+                var party = allParties.Where(p => p.Names.Any(n => comment.Contains(n))).ToList();
+                if (party.Count == 1)
+                {
+                    transaction.Party = party.Single().Party;
+                }
+            }
+        }
+
+        private static string[] GetPartyNames(Party p)
+        {
+            var result = new List<string>();
+            if (p is Organization)
+            {
+                var orgName = (((Organization)p).Name ?? string.Empty).ToLower();
+                if (!string.IsNullOrEmpty(orgName))
+                {
+
+                    result.Add(orgName);
+                    var parts = orgName.Split(' ');
+                    if (parts.Length > 1)
+                    {
+                        result.Add(string.Join(" ", parts.Take(parts.Length - 1)).Trim());
+                    }
+                }
+            }
+            else if (p is Person)
+            {
+                var person = (Person)p;
+                var firstName = (person.FirstName ?? string.Empty).ToLower();
+                var lastName = (person.LastName ?? string.Empty).ToLower();
+                var middleName = (person.MiddleName ?? string.Empty).ToLower();
+
+                result.Add(string.Join(" ", firstName, lastName).Trim());
+                result.Add(string.Join(" ", firstName, middleName, lastName).Trim());
+
+                result.Add(string.Join(" ", lastName, firstName).Trim());
+                result.Add(string.Join(" ", lastName, firstName, middleName).Trim());
+            }
+            return result.Where(s => !string.IsNullOrEmpty(s)).ToArray();
         }
     }
 }
