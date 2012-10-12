dd7c181e0083743632cd166cdac2ac0357753e49
Arthur Zaczek <arthur@dasz.at>
2012-10-12 11:51:20 +0200

    Removing unused method invocations

--- Zetbox.Parties.Common/Parties/PersonActions.cs
+++ Zetbox.Parties.Common/Parties/PersonActions.cs
@@ -21,25 +21,5 @@ namespace Zetbox.Basic.Parties
         {
             e.Result = string.Format("{0} {1}", obj.FirstName, obj.LastName);
         }
-
-        [Invocation]
-        public static void get_LastName(Zetbox.Basic.Parties.Person obj, PropertyGetterEventArgs<System.String> e)
-        {
-        }
-
-        [Invocation]
-        public static void NotifyPreSave(Zetbox.Basic.Parties.Person obj)
-        {
-        }
-
-        [Invocation]
-        public static void postSet_FirstName(Zetbox.Basic.Parties.Person obj, PropertyPostSetterEventArgs<System.String> e)
-        {
-        }
-
-        [Invocation]
-        public static void preSet_FirstName(Zetbox.Basic.Parties.Person obj, PropertyPreSetterEventArgs<System.String> e)
-        {
-        }
     }
 }
