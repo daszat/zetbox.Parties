15e4114c0eabb547dd2e9f1cf98d44207009c87f
Arthur Zaczek <arthur@dasz.at>
2012-11-15 17:37:44 +0100

    BA-CA Importer: New (broken, crazy) import format

--- Zetbox.Parties.Common/Accounting/AccountImporter.cs
+++ Zetbox.Parties.Common/Accounting/AccountImporter.cs
@@ -67,11 +67,11 @@ namespace Zetbox.Parties.Common.Accounting
 
     public class BACA_AccountImporter : AccountImporter
     {
-        // 0: Buchungsdatum; 1:Valutadatum; 2:Auszugsnummer; 3:Text1; 4:?; 5:Betrag; 6:EUR; 7:?
+        // 0: Buchungsdatum; 1:Valutadatum; 2:Text1; 3:Interne Notiz; 4:EUR; 5:Betrag;?
         public const int COL_CSV_BUCHUNGSDATUM = 0;
         public const int COL_CSV_VALUTADATUM = 1;
-        public const int COL_CSV_AUSZUGSNUMMER = 2;
-        public const int COL_CSV_BEZ1 = 3;
+        public const int COL_CSV_BEZ = 2;
+        public const int COL_CSV_INT_NOTIZ = 3;
         public const int COL_CSV_BETRAG = 5;
 
         protected override List<ImportedTransaction> Read(string fileName)
@@ -80,18 +80,19 @@ namespace Zetbox.Parties.Common.Accounting
 
             using (var file = new System.IO.StreamReader(fileName, Encoding.Default))
             {
+                if(!file.EndOfStream) file.ReadLine(); // Skip header
                 while (!file.EndOfStream)
                 {
                     var line = file.ReadLine();
                     var columns = line.Split(';');
 
-                    var buchungsdatum = Convert.ToDateTime(columns[COL_CSV_BUCHUNGSDATUM]);
-                    var valutadatum = Convert.ToDateTime(columns[COL_CSV_VALUTADATUM]);
-                    var auszugsnummer = columns[COL_CSV_AUSZUGSNUMMER];
-                    var bez = columns[COL_CSV_BEZ1].Trim('"');
+                    var buchungsdatum = Convert.ToDateTime(columns[COL_CSV_BUCHUNGSDATUM].Replace('/', '.'));
+                    var valutadatum = Convert.ToDateTime(columns[COL_CSV_VALUTADATUM].Replace('/', '.'));
+                    var bez = columns[COL_CSV_BEZ].Trim('"');
+                    var notiz = columns[COL_CSV_INT_NOTIZ].Trim('"');
                     var betrag = Convert.ToDecimal(columns[COL_CSV_BETRAG]);
 
-                    result.Add(new ImportedTransaction() { Date = valutadatum, Text = bez.Trim(), Ammount = betrag });                    
+                    result.Add(new ImportedTransaction() { Date = valutadatum, Text = bez.Trim() + "\n" + notiz.Trim(), Ammount = betrag });                    
                 }
             }
             return result;
